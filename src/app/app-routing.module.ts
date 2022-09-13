72e4ee59eca4bda853e93075babdf237992d36ae
MGamezG <l171130314@gamadero.tecnm.mx>
2022-09-13 12:53:37 -0500

    boostrap, animated, fontawesome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'heroes',
+    component:HeroesComponent
+  },
+  {
+    path:'heroe/:id',
+    component:HeroeComponent
+  },
+  {
+    path:'',
+    redirectTo:'/heroes',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
